dd7965cc1264a664943e3447d9542cb588f34e81
seyi.idowu <shezi357412@icloud.com>
2025-11-15 12:59:45 +0100

    Expand network support to include Optimism and Polygon

--- config/index.tsx
+++ config/index.tsx
@@ -1,15 +1,16 @@
 import { cookieStorage, createStorage, http } from '@wagmi/core'
 import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
-import { mainnet, arbitrum, base } from '@reown/appkit/networks'
+import { mainnet, arbitrum, base, optimism, polygon } from '@reown/appkit/networks'
 
-// Get projectId from https://dashboard.reown.com
+// Get projectId from https://cloud.reown.com
 export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID
 
 if (!projectId) {
   throw new Error('Project ID is not defined')
 }
 
-export const networks = [mainnet, arbitrum, base]
+// Supported networks for BuilderProof
+export const networks = [base, mainnet, arbitrum, optimism, polygon]
 
 //Set up the Wagmi Adapter (Config)
 export const wagmiAdapter = new WagmiAdapter({
