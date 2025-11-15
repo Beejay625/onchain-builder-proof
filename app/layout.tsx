620c37768a7788a77567646ddcab9b5291809c9f
seyi.idowu <shezi357412@icloud.com>
2025-11-15 12:02:44 +0100

    feat: set up root layout with AppKit provider and global styles

---
+++ app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import { headers } from 'next/headers'
+import ContextProvider from '@/context'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Onchain Builder Proof',
+  description: 'Mint your weekly achievements on Talenty Protocol'
+}
+
+export default async function RootLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  const headersObj = await headers()
+  const cookies = headersObj.get('cookie')
+
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ContextProvider cookies={cookies}>{children}</ContextProvider>
+      </body>
+    </html>
+  )
+}
+
