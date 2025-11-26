338415cc6d3c91b1c743609ddd7800fb4863bf1a
seyi.idowu <shezi357412@icloud.com>
2025-11-26 09:44:59 +0100

    Add OnchainAchievementCrisisBudgetRouter

---
+++ components/onchain/OnchainAchievementCrisisBudgetRouter.tsx
@@ -0,0 +1,57 @@
+'use client'
+
+import HeliosMeshComposer, { HeliosMeshFeatureProps } from './HeliosMeshComposer'
+
+export default function OnchainAchievementCrisisBudgetRouter(props: HeliosMeshFeatureProps) {
+  return (
+    <HeliosMeshComposer
+      {...props}
+      title="Crisis Budget Router"
+      subtitle="Pre-approve emergency spend"
+      accent="teal"
+      ctaLabel="Route Budget"
+      payloadPrefix="CRISIS_BUDGET"
+      fields=[
+      {
+              key: 'token',
+              label: 'Token',
+              type: 'select',
+              options: [
+          { label: 'USDC', value: 'USDC' },
+          { label: 'ETH', value: 'ETH' },
+          { label: 'OP', value: 'OP' },
+        ],
+              required: true,
+            },
+      {
+              key: 'cap',
+              label: 'Cap',
+              type: 'number',
+              placeholder: '10000',
+              required: true,
+            },
+      {
+              key: 'squad',
+              label: 'Squad',
+              type: 'text',
+              placeholder: 'treasury-core',
+              required: true,
+            },
+      {
+              key: 'reviewer',
+              label: 'Reviewer',
+              type: 'text',
+              placeholder: 'guardian.eth',
+              required: true,
+            },
+      {
+              key: 'reason',
+              label: 'Reason',
+              type: 'textarea',
+              placeholder: 'How funds will be used',
+              required: true,
+            },
+    ]
+    />
+  )
+}
