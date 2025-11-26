1b11508499442660f36c0e821bec6004ed643ea4
seyi.idowu <shezi357412@icloud.com>
2025-11-26 09:44:59 +0100

    Add OnchainAchievementGuardianAftercarePortal

---
+++ components/onchain/OnchainAchievementGuardianAftercarePortal.tsx
@@ -0,0 +1,52 @@
+'use client'
+
+import HeliosMeshComposer, { HeliosMeshFeatureProps } from './HeliosMeshComposer'
+
+export default function OnchainAchievementGuardianAftercarePortal(props: HeliosMeshFeatureProps) {
+  return (
+    <HeliosMeshComposer
+      {...props}
+      title="Guardian Aftercare Portal"
+      subtitle="Track aftercare promises"
+      accent="green"
+      ctaLabel="Log Aftercare"
+      payloadPrefix="AFTERCARE_PORTAL"
+      fields=[
+      {
+              key: 'guardian',
+              label: 'Guardian',
+              type: 'text',
+              placeholder: 'guardian.eth',
+              required: true,
+            },
+      {
+              key: 'task',
+              label: 'Aftercare Task',
+              type: 'textarea',
+              placeholder: 'Rest period, counseling, etc.',
+              required: true,
+            },
+      {
+              key: 'dueDate',
+              label: 'Due Date',
+              type: 'text',
+              placeholder: '2025-01-20',
+              required: true,
+            },
+      {
+              key: 'proofHash',
+              label: 'Proof Hash',
+              type: 'text',
+              placeholder: '0xproof',
+            },
+      {
+              key: 'owner',
+              label: 'Owner',
+              type: 'text',
+              placeholder: 'ops-care',
+              required: true,
+            },
+    ]
+    />
+  )
+}
